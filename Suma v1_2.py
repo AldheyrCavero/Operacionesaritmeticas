3556dd960265f6801d6314b42d1b4ac510261b89
62060023 <62060023@continental.edu.pe>
2024-09-02 18:50:44 -0500

    Proyecto de suma v1_2

---
+++ Suma v1_2.py
@@ -0,0 +1,17 @@
+
+class OperacionesAritmeticas():
+
+    def ingresoNumeros(self):
+        numero1 = float(input("Ingrese el primer sumando: "))
+        numero2 = float(input("Ingrese el segundo sumando: "))
+        return numero1, numero2
+
+
+    def suma(self, numero1, numero2):
+        return numero1 + numero2
+
+
+if __name__ == '__main__':
+    operación = OperacionesAritmeticas()
+    num1, num2 = operación.ingresoNumeros()
+    print(f"{num1} + {num2} = {operación.suma(num1, num2)}")
